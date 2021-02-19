5c386535f1927d368c135a182128b650ee3d7895
Ilya Komzarev <ilya.komzarev@gmail.com>
2021-02-19 15:21:49 +0300

    Исправлена сборка под линукс
    DES-587

--- serverdiscovery/string_format.h
+++ serverdiscovery/string_format.h
@@ -8,8 +8,10 @@ namespace rl
 
 namespace str
 {
+#if __cplusplus >= 201703L
     namespace details
     {
+
         template <typename T>
         auto convert(T&& t)
         {
@@ -31,7 +33,6 @@ namespace str
             snprintf(buf.get(), size, format.c_str(), args...);
             return std::string(buf.get(), buf.get() + size - 1);
         }
-
     }
 
     template <typename... Args>
@@ -39,6 +40,18 @@ namespace str
     {
         return details::stringFormatInternal(fmt, details::convert(std::forward<Args>(args))...);
     }
+#else
+    template <typename... Args>
+    inline std::string format(std::string format, Args&&... args)
+    {
+        size_t size = snprintf(nullptr, 0, format.c_str(), std::forward<Args>(args)...) + 1;
+        if (size <= 0) {
+            return {};
+        }
+        std::unique_ptr<char[]> buf(new char[size]);
+        snprintf(buf.get(), size, format.c_str(), args...);
+        return std::string(buf.get(), buf.get() + size - 1);
+    }
+#endif
 }
-
 }
