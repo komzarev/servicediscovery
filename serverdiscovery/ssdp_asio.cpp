9cdbef4507ce1236462b0b4f9d53a55f79112e69
Ilya Komzarev <ilya.komzarev@gmail.com>
2021-02-26 16:40:33 +0300

    Переделан клиент asio
    DES-587

--- serverdiscovery/ssdp_asio.cpp
+++ serverdiscovery/ssdp_asio.cpp
@@ -67,6 +67,82 @@ void ssdp::asio::Server::start_receive(UdpSocket* socket)
     });
 }
 
+ssdp::asio::Client::Client()
+    : timer_(io_service_)
+{
+    udp::resolver resolver(io_service_);
+    udp::resolver::query query(host_name(), "");
+    udp::resolver::iterator it = resolver.resolve(query);
+
+    while (it != udp::resolver::iterator()) {
+        auto addr = (it++)->endpoint();
+        if (addr.address().is_v4()) {
+
+            try {
+                auto socket = new UdpSocket(io_service_);
+                socket->socket.open(boost::asio::ip::udp::v4());
+                socket->socket.bind(udp::endpoint(addr.address(), 0));
+                std::cout << "Succesfully bind to: " << addr.address().to_v4().to_string() << "\n";
+                sockets_.emplace_back(socket);
+            }
+            catch (boost::system::system_error& err) {
+                std::cout << err.what() << '\n';
+            }
+        }
+    }
+}
+
+std::vector<ssdp::asio::Client::ServerInfo> ssdp::asio::Client::findAllServers_(const std::string& type, const std::string& name, const std::string& details, int timeout_ms, bool onlyOne)
+{
+    std::vector<ServerInfo> ret;
+    if (!sent(type, name, details)) {
+        return ret;
+    }
+
+    timer_.expires_after(std::chrono::milliseconds(timeout_ms));
+    timer_.async_wait([this](const boost::system::error_code& error) {
+        if (!error) {
+            for (auto& s : sockets_) {
+                s->socket.cancel();
+            }
+            //                    socket_.close();
+            isRunning = false;
+        }
+    });
+
+    for (auto& s : sockets_) {
+        startRecieve_(ret, onlyOne, s.get());
+    }
+    isRunning = true;
+
+    while (isRunning) { //socket_.is_open() always true
+        io_service_.run_one();
+    }
+    return ret;
+}
+
+void ssdp::asio::Client::startRecieve_(std::vector<ssdp::asio::Client::ServerInfo>& ret, bool onlyOne, ssdp::asio::UdpSocket* socket)
+{
+    socket->socket.async_receive(boost::asio::buffer(socket->buffer), [this, &ret, onlyOne, socket](const boost::system::error_code& error, const size_t bytes_recived) {
+        if (!error || error == boost::asio::error::message_size) {
+            auto res = Response::from_string(std::string(std::begin(socket->buffer), std::begin(socket->buffer) + bytes_recived));
+            if (res.has_value()) {
+                ServerInfo si;
+                si.socketString = res->location;
+                si.name = res->servername;
+                si.type = res->servertype;
+                si.details = res->serverdetails;
+                ret.push_back(si);
+                if (onlyOne) {
+                    isRunning = false;
+                } else {
+                    startRecieve_(ret, onlyOne, socket);
+                }
+            }
+        }
+    });
+}
+
 bool ssdp::asio::Client::sent(const std::string& type, const std::string& name, const std::string& details)
 {
     Request req(type, name, details);
@@ -74,7 +150,9 @@ bool ssdp::asio::Client::sent(const std::string& type, const std::string& name, 
     std::shared_ptr<std::string> message(new std::string(req.to_string()));
     udp::endpoint endpoint(address_v4::from_string("239.255.255.250"), 1900);
     try {
-        socket_.send_to(boost::asio::buffer(*message), endpoint);
+        for (auto& s : sockets_) {
+            s->socket.send_to(boost::asio::buffer(*message), endpoint);
+        }
     }
     catch (boost::system::system_error& err) {
         std::cout << err.what() << '\n';
--- serverdiscovery/ssdp_asio.hpp
+++ serverdiscovery/ssdp_asio.hpp
@@ -12,8 +12,6 @@ namespace ssdp
 namespace asio
 {
 
-    //    using UdpSocket = boost::asio::ip::udp::socket;
-
     class UdpSocket
     {
     public:
@@ -74,16 +72,13 @@ namespace asio
             std::string socketString;
         };
 
-        Client()
-            : timer_(io_service_)
-            , socket_(io_service_)
-        {
-            socket_.open(boost::asio::ip::udp::v4());
-        }
+        Client();
 
         ~Client()
         {
-            socket_.close();
+            for (auto& s : sockets_) {
+                s->socket.close();
+            }
         }
 
         std::string findConnetionString(const std::string& type, const std::string& name, const std::string& details, int timeout_ms = 500)
@@ -102,59 +97,15 @@ namespace asio
         }
 
     private:
-        std::vector<ServerInfo> findAllServers_(const std::string& type, const std::string& name, const std::string& details, int timeout_ms, bool onlyOne)
-        {
-            std::vector<ServerInfo> ret;
-            if (!sent(type, name, details)) {
-                return ret;
-            }
+        std::vector<ServerInfo> findAllServers_(const std::string& type, const std::string& name, const std::string& details, int timeout_ms, bool onlyOne);
 
-            timer_.expires_after(std::chrono::milliseconds(timeout_ms));
-            timer_.async_wait([this](const boost::system::error_code& error) {
-                if (!error) {
-                    socket_.cancel();
-                    //                    socket_.close();
-                    isRunning = false;
-                }
-            });
-
-            startRecieve_(ret, onlyOne);
-            isRunning = true;
-
-            while (isRunning) { //socket_.is_open() always true
-                io_service_.run_one();
-            }
-            return ret;
-        }
-
-        void startRecieve_(std::vector<ServerInfo>& ret, bool onlyOne = false)
-        {
-            socket_.async_receive(boost::asio::buffer(buffer_), [this, &ret, onlyOne](const boost::system::error_code& error, const size_t bytes_recived) {
-                if (!error || error == boost::asio::error::message_size) {
-                    auto res = Response::from_string(std::string(std::begin(buffer_), std::begin(buffer_) + bytes_recived));
-                    if (res.has_value()) {
-                        ServerInfo si;
-                        si.socketString = res->location;
-                        si.name = res->servername;
-                        si.type = res->servertype;
-                        si.details = res->serverdetails;
-                        ret.push_back(si);
-                        if (onlyOne) {
-                            isRunning = false;
-                        } else {
-                            startRecieve_(ret, onlyOne);
-                        }
-                    }
-                }
-            });
-        }
+        void startRecieve_(std::vector<ServerInfo>& ret, bool onlyOne, UdpSocket* socket);
         bool sent(const std::string& type, const std::string& name, const std::string& details);
 
-        std::array<char, 1024> buffer_;
         bool isRunning = false;
         boost::asio::io_service io_service_;
         boost::asio::steady_timer timer_;
-        boost::asio::ip::udp::socket socket_;
+        UdpSockets sockets_;
     };
 }
 }
